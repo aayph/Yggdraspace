c76a7c830cb55bf8786e0fe854df81ef48d4bdb4
Mara Mehlitz <mmehlitz@gmail.com>
2023-02-03 21:54:08 +0100

    adding resources

---
+++ Assets/Scripts/Resources.cs
@@ -0,0 +1,14 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class Resources
+{
+    public float organic;
+    public float metal;
+    public float water;
+}
