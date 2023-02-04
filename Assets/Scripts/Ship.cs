a02d941b6b6500fabcbd0f14c9145bcbcef51064
Stefan Gerstner <stefan.gerstner96@web.de>
2023-02-04 14:35:57 +0100

    Better shp Movement

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -4,6 +4,18 @@ public class Ship : MonoBehaviour
 {
     public Storage storage;
 
+    // Ship Statsa
+    public float EnergyConsumptionrate = 1;
+    public float TravelSpeed = 1;
+
+    // For Traveling
+    private bool IsTraveling;
+    private float TravelTimer;
+    private Vector3 travelDirection;
+
+    // For Orbiting
+    private GameObject closestPlanet;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +31,15 @@ public class Ship : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // TODO remove pseudo Code
-        if (Input.GetKey(KeyCode.Space) == true)
+        if (IsTraveling)
         {
-            EventManager.TravelTargetSelected(new Vector3());
+            transform.position = transform.position + travelDirection * TravelSpeed * Time.deltaTime;
+            storage.resources.water -= EnergyConsumptionrate * Time.deltaTime;
+            TravelTimer -= Time.deltaTime;
+            if (TravelTimer <= 0)
+            {
+                FinsishTravel();
+            }
         }
     }
 
@@ -45,8 +62,56 @@ public class Ship : MonoBehaviour
         GameStates.isInTravelSelection = false;
         GameStates.traveler = null;
 
-        // ToDo Not instant Travel
-        transform.position = target;
+        // Set some later used values
+        target = new Vector3(target.x, 1, target.z);
+        float distance = Vector3.Distance(target, transform.position);
+
+        // Set Travel Values
+        IsTraveling = true;
+        travelDirection = (target - transform.position).normalized; 
+        TravelTimer = distance / TravelSpeed;
+
+        // Do not travel if it would cost more energy then stored
+        if (TravelTimer * EnergyConsumptionrate > storage.resources.water)
+        {
+            // TODO Pop Up Can't afford this journey
+            ResetTravelValues();
+            Debug.Log("Can't afford this journey");
+        }       
     }
 
+    private void FinsishTravel()
+    {
+        if (closestPlanet != null)
+        {
+            // WIP
+        }
+        ResetTravelValues();
+    }
+
+    private void ResetTravelValues()
+    {
+        IsTraveling = false;
+        TravelTimer = 0;
+        travelDirection = Vector3.zero;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Planet")
+        {
+            closestPlanet = gameObject;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Planet")
+        {
+            if (closestPlanet == gameObject)
+            {
+                closestPlanet = null;
+            }            
+        }
+    }
 }
